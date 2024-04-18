1db131958fdaca993989cce07212a5c8dc560fad
David Yin <29613693+E137299@users.noreply.github.com>
2024-04-18 13:09:56 -0500

    pushing boxes

--- pusingboxes.py
+++ pusingboxes.py
@@ -87,59 +87,9 @@ class Box(Turtle):
         self.shapesize(2)
         self.goto(random.randint(-200,200),random.randint(-200,200))
         self.showturtle()
-        self.direction = "right"
 
-    def touch(self, box):
-        # self sides
-        pTop = self.ycor()+20
-        pRight = self.xcor()+20
-        pBottom = self.ycor()-20
-        pLeft = self.xcor()-20
 
-        # Box sides
-        bTop = box.ycor()+20
-        bRight = box.xcor()+20
-        bBottom = box.ycor()-20
-        bLeft = box.xcor()-20
 
-       # pushing right
-        if (pRight > bLeft) and (pTop>bBottom) and (pBottom<bTop) and (pLeft<bLeft):
-            return True
-       # pushing left
-        elif (pLeft < bRight) and (pTop>bBottom) and (pBottom<bTop) and (pRight>bRight):
-            return True
-       # pushing up
-        elif (pTop>bBottom) and (pRight>bLeft) and (pLeft<bRight) and (pBottom<bBottom):
-            return True
-       # pushing down
-        elif (pBottom<bTop) and (pRight>bLeft) and (pLeft<bRight) and (pTop>bTop):
-            return True
-        else:
-            return False
-        
-    def push(self,box):
-        if self.direction == "up":
-            box.sety(self.ycor()+40)
-            box.direction = "up"
-        if self.direction == "down":
-            box.sety(self.ycor()-40)
-            box.direction = "down"
-        if self.direction == "right":
-            box.setx(self.xcor()+40)
-            box.direction = "right"
-        if self.direction == "left":
-            box.setx(self.xcor()-40)
-            box.direction = "left"
-
-def foo(list, index):
-    if index < len(list):
-        for box in list:
-            if list[index] != box and list[index].touch(box):
-                box.color("white")
-                list[index].push(box)
-        return foo(list,index+1)
-    else:
-        return "done"
 
 
 ######## Driver Code ##########
@@ -161,19 +111,6 @@ while True:
         if player.touch(box):
             box.color("white")
             player.push(box)
-            foo(boxes,0)
-            # for other in boxes:
-            #     if box != other and box.touch(other):
-            #         other.color("white")
-            #         box.push(other)
-            #         for otro in boxes:
-            #             if otro != other and other.touch(otro):
-            #                 otro.color("white")
-            #                 other.push(otro)
-            #                 for last in boxes:
-            #                     if otro != last and otro.touch(last):
-            #                         last.color("white")
-            #                         otro.push(last)
         else:
             box.color("red")
         
